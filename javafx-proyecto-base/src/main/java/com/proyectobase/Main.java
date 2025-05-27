94394d919c1af28c340374f42aab5e806004ee99
santigm12 <159147660+santigm12@users.noreply.github.com>
2025-05-27 19:43:54 +0200

    Cambios en el nombre del proyecto

--- javafx-proyecto-base/src/main/java/com/proyectobase/Main.java
+++ javafx-proyecto-base/src/main/java/com/proyectobase/Main.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class PlantillaFX extends Application {
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
